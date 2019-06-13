c8598eea4431893c3f3a06b76340940008773af2
Prathik Jain <prathik862pj@live.com>
2019-06-13 17:12:29 +0530

    remove unwanted file

--- lib/core/view_models/more_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MoreModel extends ChangeNotifier {
-  void setAdvancedOptionsPref(bool value) {}
-
-  bool getAdvancedOptionsPref() {
-    return true;
-  }
-}

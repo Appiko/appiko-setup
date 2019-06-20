e334c5aa2a8f23e0df9ca5d0f8aae3db313bb405
Prathik Jain <pratik862pj@live.com>
2019-06-20 13:35:21 +0530

    :hammer: Refactor

--- lib/core/services/bluetooth_scan.dart
+++ lib/core/services/bluetooth_scan.dart
@@ -1,35 +1,55 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter/widgets.dart';
 
 class BluetoothScanService extends ChangeNotifier {
-  FlutterBlue flutterBlue = FlutterBlue.instance;
-  var _scanSubscription;
-  bool _isScanning = false;
-
-  Map<DeviceIdentifier, ScanResult> _scanResults = {};
-
-  bool get isScanning => _isScanning;
-  Map<DeviceIdentifier, ScanResult> get scanResults => _scanResults;
-
+  FlutterBlue _flutterBlue = FlutterBlue.instance;
   Duration _scanTimeoutDuration = Duration(seconds: 15);
 
+  var _scanSubscription;
+  var _bluetoothStateSubscription;
+
+  Map<DeviceIdentifier, ScanResult> scanResults = {};
+  bool isScanning = false;
+  bool isBluetoothOn = true;
+
+  BluetoothScanService() {
+    bluetoothStateListner();
+  }
+
   startScan() {
-    _isScanning = true;
-    _scanResults.clear();
-    _scanSubscription =
-        flutterBlue.scan(timeout: _scanTimeoutDuration).listen((ScanResult s) {
-      if (s.advertisementData.localName != "") scanResults[s.device.id] = s;
-      notifyListeners();
-    }, onDone: () {
-      stopScan();
-    });
+    isScanning = true;
+    scanResults.clear();
     notifyListeners();
+    _scanSubscription = _flutterBlue.scan(timeout: _scanTimeoutDuration)
+      ..listen(
+        (ScanResult s) {
+          if (s.advertisementData.localName != "") {
+            scanResults[s.device.id] = s;
+            notifyListeners();
+          }
+        },
+        onDone: () {
+          stopScan();
+        },
+      );
   }
 
   stopScan() {
+    /// TODO: cancel [_bluetoothStateSubscroption]
     _scanSubscription?.cancel();
     _scanSubscription = null;
-    _isScanning = false;
+    isScanning = false;
     notifyListeners();
+  }
+
+  void bluetoothStateListner() {
+    _bluetoothStateSubscription = _flutterBlue.onStateChanged()
+      ..listen(
+        (bluetoothState) {
+          isBluetoothOn = bluetoothState == BluetoothState.on;
+          print(bluetoothState);
+          notifyListeners();
+        },
+      );
   }
 }

29d043a2afe13e6779cdd9b27922f6917eb334bf
Prathik Jain <pratik862pj@live.com>
2019-06-21 13:00:06 +0530

    Open Settings page on iOS

--- lib/core/services/bluetooth_scan.dart
+++ lib/core/services/bluetooth_scan.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:android_intent/android_intent.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:flutter/widgets.dart';
+import 'package:system_setting/system_setting.dart';
 
 class BluetoothScanService extends ChangeNotifier {
   FlutterBlue _flutterBlue = FlutterBlue.instance;
@@ -67,16 +68,13 @@ class BluetoothScanService extends ChangeNotifier {
     scanResults.clear();
     stopScan();
     if (Platform.isAndroid) {
-      // This will trigger a dialog ("Application wants to turn on Bluetooth, do you want to allow it?")
       const intent = AndroidIntent(
         action: 'android.bluetooth.adapter.action.REQUEST_ENABLE',
       );
       await intent.launch();
-
-      // We wait 5 seconds - fixed.
-      // This is a tricky bit, because if the user waits on the aforementioned dialog for more than 5 seconds, we still have a disabled bluetooth adapter below.
-      // Also can not wait forever, so prepare your UI to switch back to a "un-connected" status.
-      await Future.delayed(Duration(seconds: 5));
+    }
+    if (Platform.isIOS) {
+      SystemSetting.goto(SettingTarget.BLUETOOTH);
     }
   }
 }

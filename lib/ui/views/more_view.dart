dde775833ab2883cc43ccf5e955011bde7d7e2dd
Prathik Jain <pratik862pj@live.com>
2019-06-21 12:59:56 +0530

    :hammer: Fix nested scroll on smaller screens

--- lib/ui/views/more_view.dart
+++ lib/ui/views/more_view.dart
@@ -39,32 +39,27 @@ class MoreView extends StatelessWidget {
             ),
           ),
           CustomDivider(),
-          NotificationListener<OverscrollIndicatorNotification>(
-            onNotification: (overscroll) {
-              overscroll.disallowGlow(); //Hides the glow on overscroll
-            },
-            child: ListView.separated(
-              shrinkWrap: true,
-              scrollDirection: Axis.vertical,
-              separatorBuilder: (context, int) => CustomDivider(),
-              itemBuilder: (context, int index) => ListTile(
-                    title: Text(moreList.keys.toList()[index]),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        //TODO: Make indivisual pages and add it to routes in main.dart
-                        MaterialPageRoute(
-                          builder: (context) => Scaffold(
-                                appBar: CustomAppBar(
-                                    title: moreList.keys.toList()[index]),
-                                body: moreList.values.toList()[index],
-                              ),
-                        ),
-                      );
-                    },
-                  ),
-              itemCount: moreList.length,
-            ),
+          ListView.separated(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            separatorBuilder: (context, int) => CustomDivider(),
+            itemBuilder: (context, int index) => ListTile(
+                  title: Text(moreList.keys.toList()[index]),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      //TODO: Make indivisual pages and add it to routes in main.dart
+                      MaterialPageRoute(
+                        builder: (context) => Scaffold(
+                              appBar: CustomAppBar(
+                                  title: moreList.keys.toList()[index]),
+                              body: moreList.values.toList()[index],
+                            ),
+                      ),
+                    );
+                  },
+                ),
+            itemCount: moreList.length,
           ),
         ],
       ),

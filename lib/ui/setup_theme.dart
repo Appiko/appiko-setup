570204beca4c8e26c95c686311476bd8c00dcc7f
Prathik Jain <pratik862pj@live.com>
2019-07-03 17:32:55 +0530

    :rocket: Edit global theme

--- lib/ui/setup_theme.dart
+++ lib/ui/setup_theme.dart
@@ -1,86 +1,199 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
+import 'package:provider/provider.dart';
+
+import 'colors.dart';
 
 class SetupTheme with ChangeNotifier {
-  static Color _oneColor = Color(0xFFE06B1E);
-  static Color _black = Color(0xFF1F1F1F);
-  static Color _backgroundLight = Color(0xFFEDF0F2);
-  static FontWeight _medium = FontWeight.w700;
-  static TextTheme _defaultTextTheme = TextTheme(
-    title: TextStyle(fontSize: 34, fontWeight: _medium, color: _black),
-  );
+  // static FontWeight _medium = FontWeight.w700;
+  // static TextTheme _defaultTextTheme = TextTheme(
+  //   title: TextStyle(fontSize: 34, fontWeight: _medium, color: _black),
+  // );
 
   // static Color _backgroundDark = Color(0xFFF1F1F1);
   // static Color _black = Color(0xFFF1F1F1);
 
-  // Default Theme
-  ThemeData appikoDefualtTheme = ThemeData(
-    textTheme: TextTheme(
-      body1: TextStyle(
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
-        color: Colors.black,
-      ),
-      body2: TextStyle(
-        fontSize: 14,
-        color: _black,
-      ),
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      border: OutlineInputBorder(),
-    ),
-    brightness: Brightness.light,
-    primaryColor: _black,
-    accentColor: _oneColor,
-    backgroundColor: _oneColor.withAlpha(100),
-    floatingActionButtonTheme: FloatingActionButtonThemeData(
-      backgroundColor: _black,
-    ),
-    cursorColor: _oneColor,
-    scaffoldBackgroundColor: _backgroundLight,
-    appBarTheme: AppBarTheme(
-      iconTheme: IconThemeData(color: _black),
-      color: Colors.transparent,
-      elevation: 0,
-      textTheme: _defaultTextTheme,
-    ),
-  );
+  final ThemeData appikoLightTheme = _buildLightTheme();
+  final ThemeData appikoDarkTheme = _buildDarkTheme();
 
-  // Dark Theme
-  ThemeData appikoDarkTheme = ThemeData(
-    brightness: Brightness.dark,
-    toggleableActiveColor: _oneColor,
-    backgroundColor: _black.withAlpha(100),
-    accentColor: _oneColor,
-    floatingActionButtonTheme: FloatingActionButtonThemeData(
-      backgroundColor: Colors.white,
-      foregroundColor: _black,
-    ),
-    cursorColor: _oneColor,
-    textTheme: TextTheme(
-      body1: TextStyle(
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
-        color: Colors.white,
+  static ThemeData _buildLightTheme() {
+    final ThemeData base = ThemeData.light();
+    return ThemeData(
+      brightness: Brightness.light,
+      primaryColor: black,
+      primaryColorDark: black,
+      primaryColorLight: black.withAlpha(100),
+      scaffoldBackgroundColor: backgroundLight,
+      backgroundColor: oneColor.withAlpha(100),
+      cardColor: surfaceLight,
+      textSelectionColor: oneColor.withAlpha(100),
+      errorColor: error,
+      accentColor: oneColor,
+      toggleableActiveColor: oneColor,
+      cursorColor: oneColor,
+      indicatorColor: oneColor,
+      canvasColor: Colors.white,
+      primaryColorBrightness: Brightness.dark,
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: black,
       ),
-      body2: TextStyle(
-        fontSize: 14,
-        color: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(),
       ),
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      border: OutlineInputBorder(),
-    ),
-    appBarTheme: AppBarTheme(
-      iconTheme: IconThemeData(color: Colors.white),
-      color: Colors.transparent,
-      elevation: 0,
-      textTheme: _defaultTextTheme.copyWith(
-        title: TextStyle(
-          fontSize: 34,
-          fontWeight: _medium,
-          color: Colors.white,
+      hintColor: black,
+      iconTheme: IconThemeData(color: black),
+      appBarTheme: AppBarTheme(
+        iconTheme: IconThemeData(color: black),
+        color: Colors.transparent,
+        elevation: 0,
+        textTheme: TextTheme().copyWith(
+          title: TextStyle(
+            fontSize: 34,
+            fontWeight: FontWeight.w700,
+            color: black,
+          ),
         ),
       ),
-    ),
-  );
+      textTheme: _buildTextTheme(base.textTheme),
+      accentTextTheme: _buildTextTheme(base.accentTextTheme),
+      primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
+    );
+  }
+
+  static ThemeData _buildDarkTheme() {
+    final ThemeData base = ThemeData.dark();
+    return ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.white,
+      primaryColorDark: Colors.white,
+      primaryColorLight: Colors.white.withAlpha(100),
+      backgroundColor: oneColor.withAlpha(100),
+      textSelectionColor: oneColor.withAlpha(100),
+      errorColor: error,
+      accentColor: oneColor,
+      toggleableActiveColor: oneColor,
+      cursorColor: oneColor,
+      indicatorColor: oneColor,
+      primaryColorBrightness: Brightness.dark,
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: Colors.white,
+        foregroundColor: black,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.white,
+            width: 2.0,
+          ),
+        ),
+      ),
+      iconTheme: IconThemeData(color: Colors.white),
+      appBarTheme: AppBarTheme(
+        iconTheme: IconThemeData(color: Colors.white),
+        color: Colors.transparent,
+        elevation: 0,
+        textTheme: TextTheme().copyWith(
+          title: TextStyle(
+            fontSize: 34,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
+      textTheme: _buildTextTheme(base.textTheme),
+      accentTextTheme: _buildTextTheme(base.accentTextTheme),
+      primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
+    );
+  }
+
+  static TextTheme _buildTextTheme(TextTheme base) {
+    return base.copyWith(
+      headline: base.headline.copyWith(
+        fontWeight: FontWeight.w500,
+      ),
+      title: base.title.copyWith(
+        fontSize: 18.0,
+        fontWeight: FontWeight.w700,
+      ),
+      body1: base.body1.copyWith(
+        fontSize: 16,
+        fontWeight: FontWeight.normal,
+      ),
+      caption: base.caption.copyWith(
+        fontWeight: FontWeight.w400,
+        fontSize: 14.0,
+      ),
+    );
+  }
+  // Default Theme
+  // ThemeData appikoDefualtTheme = ThemeData(
+  //   textTheme: TextTheme(
+  //     body1: TextStyle(
+  //       fontSize: 18,
+  //       fontWeight: FontWeight.bold,
+  //       color: Colors.black,
+  //     ),
+  //     body2: TextStyle(
+  //       fontSize: 14,
+  //       color: _black,
+  //     ),
+  //   ),
+  //   inputDecorationTheme: InputDecorationTheme(
+  //     border: OutlineInputBorder(),
+  //   ),
+  //   brightness: Brightness.light,
+  //   primaryColor: _black,
+  //   accentColor: _oneColor,
+  //   backgroundColor: _oneColor.withAlpha(100),
+  //   floatingActionButtonTheme: FloatingActionButtonThemeData(
+  //     backgroundColor: _black,
+  //   ),
+  //   cursorColor: _oneColor,
+  //   scaffoldBackgroundColor: _backgroundLight,
+  //   appBarTheme: AppBarTheme(
+  //     iconTheme: IconThemeData(color: _black),
+  //     color: Colors.transparent,
+  //     elevation: 0,
+  //     textTheme: _defaultTextTheme,
+  //   ),
+  // );
+
+  // Dark Theme
+  // ThemeData appikoDarkTheme = ThemeData(
+  //   brightness: Brightness.dark,
+  //   toggleableActiveColor: _oneColor,
+  //   backgroundColor: _black.withAlpha(100),
+  //   accentColor: _oneColor,
+  //   floatingActionButtonTheme: FloatingActionButtonThemeData(
+  //     backgroundColor: Colors.white,
+  //     foregroundColor: _black,
+  //   ),
+  //   cursorColor: _oneColor,
+  //   textTheme: TextTheme(
+  //     body1: TextStyle(
+  //       fontSize: 18,
+  //       fontWeight: FontWeight.bold,
+  //       color: Colors.white,
+  //     ),
+  //     body2: TextStyle(
+  //       fontSize: 14,
+  //       color: Colors.white,
+  //     ),
+  //   ),
+  //   inputDecorationTheme: InputDecorationTheme(
+  //     border: OutlineInputBorder(),
+  //   ),
+  //   appBarTheme: AppBarTheme(
+  //     iconTheme: IconThemeData(color: Colors.white),
+  //     color: Colors.transparent,
+  //     elevation: 0,
+  //     textTheme: _defaultTextTheme.copyWith(
+  //       title: TextStyle(
+  //         fontSize: 34,
+  //         fontWeight: _medium,
+  //         color: Colors.white,
+  //       ),
+  //     ),
+  //   ),
+  // );
 }

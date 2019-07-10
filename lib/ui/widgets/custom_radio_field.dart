f8b8648a5403e9d0a112d988b1e0da4d9ec05da5
Prathik <pratik862pj@live.com>
2019-07-10 18:38:55 +0530

    :sparkle: CustomRadioField

---
+++ lib/ui/widgets/custom_radio_field.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+///Example
+///
+///  ```dart
+///    CustomRadioField(
+///      title: "Video recording starts on",
+///      description:
+///          "Does your camera start recording video on full press or a half press?",
+///      radioList: ListView(
+///        children: <Widget>[
+///          RadioListTile(
+///            title: Text("Full Press"),
+///            value: 0,
+///            groupValue: 0,
+///            onChanged: (int value) {},
+///          ),
+///          RadioListTile(
+///            title: Text("Full Press"),
+///            value: 1,
+///            groupValue: 0,
+///            onChanged: (int value) {},
+///          ),
+///        ],
+///      ),
+///    );
+///  ```
+
+class CustomRadioField extends StatelessWidget {
+  final String title;
+  final String description;
+  final ListView radioList;
+
+  const CustomRadioField({
+    Key key,
+    @required this.title,
+    @required this.description,
+    @required this.radioList,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          title,
+          style: Theme.of(context).textTheme.title.copyWith(fontSize: 24.0),
+        ),
+        Text(
+          description,
+          style: Theme.of(context).textTheme.body2,
+        ),
+        SizedBox(height: 10),
+        Flexible(child: radioList),
+      ],
+    );
+  }
+}

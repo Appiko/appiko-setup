fd99e3343f75a3736c3b677b5267c0ddcf7fcaa9
Prathik <pratik862pj@live.com>
2019-07-11 19:01:19 +0530

    :sparkle: Camera Trigger options view screen

---
+++ lib/ui/devices/sense_be/1.0/settings/camera_trriger_viewe.dart
@@ -0,0 +1,40 @@
+// TODO: Place file in proper path
+
+import 'package:flutter/material.dart';
+import 'package:setup/ui/widgets/bottom_action_bar.dart';
+import 'package:setup/ui/widgets/camera_trigger_radio_options.dart';
+import 'package:setup/ui/widgets/custom_app_bar.dart';
+import 'package:setup/ui/widgets/page_navigation_bar.dart';
+
+class CameraTriggerView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: CustomAppBar(
+        title: "Camera Trigger",
+        downArrow: true,
+        onDownArrowPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: CameraTriggerRadioOptions(
+              selectedAction: CameraAction.SINGLE_PICTURE,
+            ),
+          ),
+          PageNavigationBar(
+            showNext: true,
+            showPrevious: false,
+            onNext: () {
+              // TODO: Add proper path to navigate
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
